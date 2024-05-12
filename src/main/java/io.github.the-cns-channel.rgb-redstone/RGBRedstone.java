bc160c345247a33172993198f14679257bb37b13
lukas <lukas@LENOVOLEGION5>
2024-05-12 18:39:24 -0500

    Fix 2 Files; Create ItemInit.java

--- src/main/java/io.github.the-cns-channel.rgb-redstone/RGBRedstone.java
+++ src/main/java/io.github.the-cns-channel.rgb-redstone/RGBRedstone.java
@@ -1,4 +1,4 @@
-package io.github.the-cns-channel.rgb-redstone;
+package (io.github.the-cns-channel.rgb-redstone);
 
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModList;
@@ -8,12 +8,13 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@SuppressWarnings("removal")
 @Mod(RGBRedstone.MOD_ID)
 public class RGBRedstone {
 
   public static String MOD_ID = "rgbredstone";
   public static Logger logger = LoggerFactory.getLogger(RGBRedstone.class);
-  
+
   public RGBRedstone(IEventBus bus) {
 
     FMLJavaModLoadingContext.get().getModEventBus().addListener(FMLClientSetupEvent.class, (fmlClientSetupEvent -> {
---
+++ src/main/java/io.github.the-cns-channel.rgb-redstone/core.init/ItemInit.java
@@ -0,0 +1,11 @@
+package io.github.the-cns-channel.rgb-redstone.core.init;
+
+import io.github.the-cns-channel.rgb-redstone.RGBRedstone;
+import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.registries.DeferredItems;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ItemInit {
+    
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(RGBRedstone.MOD_ID);
+}
